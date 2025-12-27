feee672adbef5d8fbd1f63650ed819e054a5e014
AbdulsemedM <abdumussema22@gmail.com>
2025-12-27 22:16:23 +0300

    Implement file system functionality: add save and read methods, integrate path provider, and update UI for file operations

--- lib/screens/files_system.dart
+++ lib/screens/files_system.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FilesSystem extends StatefulWidget {
   const FilesSystem({super.key});
@@ -8,12 +11,68 @@ class FilesSystem extends StatefulWidget {
 }
 
 class _FilesSystemState extends State<FilesSystem> {
+  final controller = TextEditingController();
+  String content = "";
+
+  @override
+  void initState() {
+    super.initState();
+    read();
+  }
+
+  Future<File> getFile() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return File('${dir.path}/note.txt');
+  }
+
+  Future<void> save() async {
+    try {
+      final file = await getFile();
+      await file.writeAsString(controller.text);
+      print("file path: ${file.path}");
+    } catch (e) {
+      print("Error saving file: $e");
+    }
+  }
+
+  Future<void> read() async {
+    try {
+      final file = await getFile();
+      if (await file.exists()) {
+        final fileContent = await file.readAsString();
+        setState(() {
+          content = fileContent;
+          controller.text = fileContent;
+        });
+      }
+    } catch (e) {
+      print("Error reading file: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('File System')),
-      body: Center(
-        child: Text('File System Screen'),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(labelText: "Write Data"),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(onPressed: save, child: Text("Save")),
+            ElevatedButton(onPressed: read, child: Text("Read")),
+            SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text("File Content: \n$content"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
